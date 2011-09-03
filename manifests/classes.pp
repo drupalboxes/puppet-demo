0499ff69b2c74eacceac0bc4caed6e9e32ba361c
robknight <robknight@li300-112.members.linode.com>
2011-09-03 12:54:27 -0400

    Initial commit

---
+++ manifests/classes.pp
@@ -0,0 +1,28 @@
+
+class webserver::custom_php {
+  class {'php::params':
+    package        => "php",
+    package_common => "php-common",
+    memory_limit   => "129M"
+  }
+  
+  include php
+  
+  include php::apc
+  php::extension { 'mcrypt': }
+  php::extension { 'mysql': }
+  php::extension { 'pecl-memcache': }
+}
+
+class webserver {
+  include apache
+  include memcached
+  include webserver::custom_php
+  include varnish
+  include drupal
+
+  include mysql::server
+  include mysql::client
+  
+  include drush
+}
